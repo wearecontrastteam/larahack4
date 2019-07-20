90d8fa096a3c06c620753e505cf7aa05393a1992
mikeaag <mike.gatward@xenonsoft.co.uk>
2019-07-20 13:47:58 +0100

    Create initial game bits (#4)

--- app/Game.php
+++ app/Game.php
@@ -4,8 +4,47 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Game
+ *
+ * @property-read \App\User $player_one
+ * @property-read \App\User $player_two
+ * @property-read \App\GameStatus $status
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game query()
+ * @mixin \Eloquent
+ * @property int $id
+ * @property int $player_one_id
+ * @property int|null $player_two_id
+ * @property int $status_id
+ * @property int|null $current_player
+ * @property mixed|null $player_one_state
+ * @property mixed|null $player_two_state
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game whereCurrentPlayer($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game wherePlayerOneId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game wherePlayerOneState($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game wherePlayerTwoId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game wherePlayerTwoState($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game whereStatusId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game whereUpdatedAt($value)
+ * @property mixed|null $people
+ * @property int|null $player_one_person_id
+ * @property int|null $player_two_person_id
+ * @property int|null $winner_id
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game wherePeople($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game wherePlayerOnePersonId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game wherePlayerTwoPersonId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Game whereWinnerId($value)
+ */
 class Game extends Model
 {
+    protected $fillable = ['player_one_id', 'status_id'];
+
     public function player_one()
     {
         return $this->hasOne(User::class);
@@ -19,6 +58,32 @@ class Game extends Model
     public function status()
     {
         return $this->hasOne(GameStatus::class);
+    }
+
+    public function player_one_person()
+    {
+        return $this->hasOne(Person::class);
+    }
+
+    public function player_two_person()
+    {
+        return $this->hasOne(Person::class);
+    }
+
+    public function winner()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function isAwaitingOpponent()
+    {
+        return $this->status_id === GameStatus::MATCHING;
+    }
+
+    public function hasPlayer(int $id)
+    {
+        return $this->player_one_id === $id
+            || $this->player_two_id === $id;
     }
 
 }
--- app/GameStatus.php
+++ app/GameStatus.php
@@ -4,8 +4,35 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\GameStatus
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Game[] $games
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\GameStatus newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\GameStatus newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\GameStatus query()
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $status
+ * @property string $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\GameStatus whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\GameStatus whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\GameStatus whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\GameStatus whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\GameStatus whereUpdatedAt($value)
+ */
 class GameStatus extends Model
 {
+    public const CREATING = 1;
+    public const MATCHING = 2;
+    public const IN_PROGRESS = 3;
+    public const FINISHED = 4;
+    public const ERROR = 5;
+
+    protected $fillable = ['id', 'status', 'description'];
+
     public function games()
     {
         return $this->belongsToMany(Game::class);
