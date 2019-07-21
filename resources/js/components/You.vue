0f001557546ace54646e27527aa4a2ca58201db2
Simon Monk-Chipman <simon@french-property.com>
2019-07-21 10:57:42 +0100

    and you can tell everybody this is your caaard

--- resources/js/components/You.vue
+++ resources/js/components/You.vue
@@ -1,12 +1,21 @@
 <template>
     <div class="you">
-        your image goes here
+
+        <div class="your-selection">
+            <img :src="avatar_url" >
+            <span class="name">{{name}}</span>
+            <span v-if="this.bio.length != 0" class="bio">{{bio}}</span>
+        </div>
     </div>
 </template>
 
 
 <script>
     export default {
-
+        props: [
+            'avatar_url',
+            'name',
+            'bio'
+        ],
     }
 </script>
--- resources/views/game/play.blade.php
+++ resources/views/game/play.blade.php
@@ -4,6 +4,6 @@
     <div class=" col-md-12 play">
         <opponent username="Test Name 1"></opponent>
         <tiles></tiles>
-        <you></you>
+        <you avatar_url="https://avatars0.githubusercontent.com/u/13378174?s=460&v=4" name="simonm-c" bio=""></you>
     </div>
 @endsection
