d0d7ac4514d62aeac4df9dac1f5d47a9a25be6d1
simonm-c <simon@french-property.com>
2019-07-21 11:27:07 +0100

    Your card (#13)
    * and you can tell everybody this is your caaard
    
    * is *this* your card?

--- resources/js/components/You.vue
+++ resources/js/components/You.vue
@@ -1,12 +1,22 @@
 <template>
-    <div class="you">
-        your image goes here
+    <div class="you" :class="player_number">
+
+        <div class="your-selection">
+            <img :src="avatar_url" >
+            <span class="name">{{name}}</span>
+            <span v-if="this.bio.length != 0" class="bio">{{bio}}</span>
+        </div>
     </div>
 </template>
 
 
 <script>
     export default {
-
+        props: [
+            'avatar_url',
+            'name',
+            'bio',
+            'player_number'
+        ],
     }
 </script>
--- resources/views/game/play.blade.php
+++ resources/views/game/play.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class=" col-md-12 play">
         <opponent username="Test Name 1"></opponent>
-        <tiles></tiles>
-        <you></you>
+        <tiles player_number="player-1"></tiles>
+        <you player_number="player-1" avatar_url="https://avatars0.githubusercontent.com/u/13378174?s=460&v=4" name="simonm-c" bio=""></you>
     </div>
 @endsection
